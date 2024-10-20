3e3bfd6f00f0e2aa17328a558d04e7bd2eb1ac2b
SCRIPTERBLOX <scriptoblox@gmail.com>
2024-10-20 21:01:43 +0200

    Added some secret messages

---
+++ secrets.js
@@ -0,0 +1,14 @@
+console.log("Hello.");
+console.log("It seems that you have found the console.");
+console.log("Sorry to break it to you... I didn't add any cheat codes.");
+console.log("Also if you already died then you probably know the respawn screen.");
+console.log("The thing for it to say how many revive tokens you have was way more work to make say plural and everything than I really should have spent.");
+console.log("If you are interested this is the code:");
+console.log("");
+console.log("if (tokens > 1) {");
+console.log("    text('You currently have |'+tokens+''| revive tokens'', 147, 285);");
+console.log("} else if (tokens == 0) {");
+console.log("    text('You currently have no revive tokens', 147, 285);");
+console.log("} else {");
+console.log("    text('You currently have >'+tokens+'< revive token', 147, 285);")
+console.log("}");
